7542edd8b2db54f59d0b3fc2f6a5d725c590884f
PaulJalagari <paul.jalagari@imaginea.com>
2015-11-27 11:31:43 +0530

    Commit

--- src/matching/strings/files/MatchingStrings.java
+++ src/matching/strings/files/MatchingStrings.java
@@ -1,25 +1,24 @@
 package matching.strings.files;
 
-import java.io.*;
-
-import java.util.*;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class MatchingStrings {
 	public static void main(String args[]) throws IOException {
-	    Scanner sc1 = new Scanner(InputStream);
-	    Scanner sc2 = new Scanner(InputStream);
-	    while (sc1.hasNext() && sc2.hasNext()) {
-	        String str1 = sc1.next();
-	        String str2 = sc2.next();
-	        if (!str1.equals(str2))
-	            System.out.println(str1 + " != " + str2);
-	    }
-	    while (sc1.hasNext())
-	        System.out.println(sc1.next() + " != EOF");
-	    while (sc2.hasNext())
-	        System.out.println("EOF != " + sc2.next());
-	    sc1.close();
-	    sc2.close();
+		Scanner sc1 = new Scanner(System.in);
+		Scanner sc2 = new Scanner(System.in);
+		while (sc1.hasNext() && sc2.hasNext()) {
+			String str1 = sc1.next();
+			String str2 = sc2.next();
+			if (!str1.equals(str2))
+				System.out.println(str1 + " != " + str2);
+		}
+		while (sc1.hasNext())
+			System.out.println(sc1.next() + " != EOF");
+		while (sc2.hasNext())
+			System.out.println("EOF != " + sc2.next());
+		sc1.close();
+		sc2.close();
 	}
 
 }
